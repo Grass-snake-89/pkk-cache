345e918021bdf040fe3ee03a53f16fa46be706d7
Grass-snake-89 <87708311+Grass-snake-89@users.noreply.github.com>
2025-05-29 21:13:59 +0300

    Update pkk-cache.py
    nspd

--- pkk-cache.py
+++ pkk-cache.py
@@ -1,28 +1,59 @@
-from flask import Flask, request, escape, send_file
+from flask import Flask, request, send_file
+from markupsafe import escape
 import os
+from random import random
 import requests
-from PIL import Image
+import urllib3
 
 settings = {
-'land':'arcgis/rest/services/PKK6/CadastreObjects/MapServer/export?layers=show%3A21&format=PNG32',
-'building':'arcgis/rest/services/PKK6/CadastreObjects/MapServer/export?layers=show%3A30&format=PNG32',
-'boundary':'arcgis/rest/services/PKK6/BordersGKN/MapServer/export?layers=show%3A0%2C5%2C16%2C25%2C32&format=PNG32',
-'zone':'arcgis/rest/services/PKK6/Zones/MapServer/export?layers=show%3A0%2C5%2C6%2C11&format=PNG32'
+    'land': 36048,
+    'building': 36049,
 }
-
+headers = {
+    'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br, zstd',
+    'accept-language': 'ru-RU,ru;q=0.9',
+    'origin': 'https://nspd.gov.ru',
+    'priority': 'i',
+    'referer': 'https://nspd.gov.ru/map?thematic=PKK&zoom=10.6826410043574285&coordinate_x=5241461.661219545&coordinate_y=7231818.562737224&theme_id=1&is_copy_url=true&active_layers=36048',
+    'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'image',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
+}
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 pkk = Flask(__name__, static_url_path='')
 
-#Тест
+
+# Тест
 @pkk.route('/')
 def index():
     return 'It works!'
 
+
 @pkk.route('/path/<path:subpath>')
 def static_file(subpath):
     bbox = str(request.args.get('bbox'))
-    os.makedirs('cache/'+escape(subpath)+'/', exist_ok=True)
-    if not os.path.isfile('cache/'+escape(subpath)+'/'+bbox+'.png'):
-        r = requests.get('https://pkk.rosreestr.ru/'+settings[escape(subpath)]+'&bbox='+bbox+'&bboxSR=102100&imageSR=102100&size=1024%2C1024&transparent=true&f=image', verify=False)  
-        with open('cache/'+escape(subpath)+'/'+bbox+'.png', 'wb') as f:
+    os.makedirs('cache2/'+escape(subpath)+'/', exist_ok=True)
+    if not os.path.isfile('cache2/'+escape(subpath)+'/'+bbox+'.png'):
+        layer_id = settings[escape(subpath)]
+        r = requests.get('https://nspd.gov.ru/api/aeggis/v3/' + str(layer_id) + '/wms', params={
+            'REQUEST': 'GetMap',
+            'SERVICE': 'WMS',
+            'VERSION': '1.3.0',
+            'FORMAT': 'image/png',
+            'STYLES': '',
+            'TRANSPARENT': 'true',
+            'LAYERS': layer_id,
+            'RANDOM': random(),
+            'WIDTH': '512',
+            'HEIGHT': '512',
+            'CRS': 'EPSG:3857',
+            'BBOX': bbox
+        }, headers=headers, verify=False)
+        with open('cache2/'+escape(subpath)+'/'+bbox+'.png', 'wb') as f:
             f.write(r.content)
-    return send_file('cache/'+escape(subpath)+'/'+bbox+'.png', mimetype='image/png')
+    return send_file('cache2/'+escape(subpath)+'/'+bbox+'.png', mimetype='image/png')
